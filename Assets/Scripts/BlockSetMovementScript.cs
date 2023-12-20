1ea3151effc4cf9f16a3ac5312cd43c55c0d5ef3
sproutdevv <sergej2007m@gmail.com>
2023-12-20 19:23:27 +0100

    Update 2

--- Assets/Scripts/BlockSetMovementScript.cs
+++ Assets/Scripts/BlockSetMovementScript.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public GameObject BlockSet1;
+    public GameObject[] BlockSet;
     public float speed = 5f;
+    public float deadZone = -10;
 
     // Update is called once per frame
     void Update()
     {
+        BlockSet = GameObject.FindGameObjectsWithTag("BlockSet");
         transform.position += new Vector3(0, 0, -1) * speed * Time.deltaTime;
+
+        if (transform.position.z <= deadZone)
+        {
+            while (BlockSet.Length > 0)
+            {
+                Destroy(BlockSet[0]);
+            }
+        }
+    
     }
 }
--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class SpawnerScript : MonoBehaviour
 {
-    public GameObject BlockSet2;
-    public float spawnRate = 1;
-    private float timer = 0;
-    // Start is called before the first frame update
+    public float spawnRate = 1f;
+    private float timer = 0f;
+
+    // ----Array machen----
+    public GameObject [] BlockSetAll;
+
+    public GameObject instantiatedBlockSet;
     void Start()
     {
+        spawnBlockset();                                // Methode spawnBlockset wird aufgerufen
     }
 
     void Update()
     {
         if (timer < spawnRate)
         {
-            timer = timer + Time.deltaTime;
+            timer = timer + Time.deltaTime;             // timer wird mit der aktuellen Sekundenanzahl addiert, heißt (jede Sekunde = timer + 1) in dem Falle
+        }
+        else if(timer > spawnRate)
+        {
+            spawnBlockset();                            // Methode spawnBlockset wird aufgerufen
+            timer = 0;                                  // timer wird wieder auf null gesetzt um eine Schleife zu erstellen 
         }
         else
         {
-            Instantiate(BlockSet2, transform.position, transform.rotation);
-            timer = 0;
+            Debug.LogError("Keine GameObjects mit dem Tag 'BlockSet' gefunden.");
         }
+    }
 
-        
+    void spawnBlockset()
+    {
+
+        instantiatedBlockSet = Instantiate(BlockSetAll[Random.Range(0, BlockSetAll.Length)]);    //Dupliziert ein GameObject
     }
 }
